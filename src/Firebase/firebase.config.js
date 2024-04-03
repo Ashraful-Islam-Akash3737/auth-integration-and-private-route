e135125364b71d0cdbfb8ba5aed8d6cea9ae82c8
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-04-03 11:34:12 +0600

    install tailwind, daisy ui, firebase.

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCME2zELt4JVG-EKLpDvzBimwt19oEzM2k",
+  authDomain: "auth-integration-private-4eeec.firebaseapp.com",
+  projectId: "auth-integration-private-4eeec",
+  storageBucket: "auth-integration-private-4eeec.appspot.com",
+  messagingSenderId: "777286447586",
+  appId: "1:777286447586:web:0febd0914b3a0321fa49bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
